b1954e6903ddca23022aa495d1788c53f01073c7
Mark Cooper <markchristophercooper@gmail.com>
2019-06-12 17:40:46 -0700

    Re-org default

--- config/initializers/converters/_record.rb
+++ config/initializers/converters/_record.rb
@@ -1,6 +1,13 @@
 module CollectionSpace
   module Converter
     module Default
+      def self.registered_procedures
+        []
+      end
+
+      def self.registered_profiles
+        {}
+      end
 
       # used for remote actions
       # subtype is defined for authority records to target a specific authority (sub)type
