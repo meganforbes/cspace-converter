cb2714e0853086efa3629ee31497bcc122e32766
Mark Cooper <markchristophercooper@gmail.com>
2019-06-19 12:08:44 -0700

    Require default module first

--- config/initializers/collectionspace.rb
+++ config/initializers/collectionspace.rb
@@ -1,3 +1,9 @@
+# 1st require the DEFAULT module
+Dir["#{Rails.root.join('lib', 'collectionspace', 'converter', 'default')}/*.rb"].each do |lib|
+  require lib
+end
+
+# then require everything else
 Dir["#{Rails.root.join('lib', 'collectionspace')}/**/*.rb"].each do |lib|
   require lib
 end
