b1eb3748a558705845949166469475b02677eade
Mark Cooper <markchristophercooper@gmail.com>
2019-06-21 15:04:54 -0700

    Cleanup spec

--- spec/models/collection_space_object_spec.rb
+++ spec/models/collection_space_object_spec.rb
@@ -4,13 +4,14 @@ RSpec.describe CollectionSpaceObject do
 
   describe "initialization" do
     let(:authority_object) {
-      CollectionSpaceObject.create({
+      build(
+        :collection_space_object,
         category: 'Authority',
         type: 'Person',
         subtype: 'person',
         identifier: 'Mickey Mouse',
         title: 'Mickey Mouse',
-      })
+      )
     }
 
     let(:authority_object_fingerprint) {
@@ -18,12 +19,13 @@ RSpec.describe CollectionSpaceObject do
     }
 
     let(:procedure_object) {
-      CollectionSpaceObject.create({
+      build(
+        :collection_space_object,
         category: 'Procedure',
         type: 'Acquisition',
         identifier_field: 'acquisitionReferenceNumber',
         identifier: '123',
-      })
+      )
     }
 
     let(:procedure_object_fingerprint) {
@@ -33,13 +35,14 @@ RSpec.describe CollectionSpaceObject do
     }
 
     let(:relationship_object) {
-      CollectionSpaceObject.create({
+      build(
+        :collection_space_object,
         category: 'Relationship',
         csid: 'fake',
         identifier: 'xyz',
         type: 'Relationship',
         uri: '/fake',
-      })
+      )
     }
 
     let(:relationship_object_fingerprint) { nil }
@@ -71,14 +74,17 @@ RSpec.describe CollectionSpaceObject do
     end
 
     it "fingerprints authority objects correctly" do
+      authority_object.set_fingerprint
       expect(authority_object.fingerprint).to eq authority_object_fingerprint
     end
 
     it "fingerprints procedure objects correctly" do
+      procedure_object.set_fingerprint
       expect(procedure_object.fingerprint).to eq procedure_object_fingerprint
     end
 
     it "fingerprints relationship objects correctly" do
+      relationship_object.set_fingerprint
       expect(relationship_object.fingerprint).to eq relationship_object_fingerprint
     end
 
