a1353341607d3e6e043e684ad6b0837deae5759e
Mark Cooper <markchristophercooper@gmail.com>
2019-06-27 16:40:29 -0700

    Whitelist params

--- app/helpers/data_objects_helper.rb
+++ app/helpers/data_objects_helper.rb
@@ -1,5 +1,9 @@
 module DataObjectsHelper
 
+  def current_page_params
+    request.params.slice('errors')
+  end
+
   def object_label(object)
     "#{object.id} #{object.import_batch} #{object.converter_module} #{object.converter_profile}"
   end
